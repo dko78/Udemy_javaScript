4efc5be63d559f5b421d749c7e706df7b04e8342
Dalibor <kovacic.dalibor@gmail.com>
2024-01-26 15:50:20 +0100

    refaktorirano

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,2 +1,14 @@
 'use strict';
 //test
+const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('overlay');
+
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('show-modal');
+
+for (let index = 0; index < btnsOpenModal.length; index++) {
+  //const element = array[index];
+  console.log(btnsOpenModal[index].textContent);
+}
