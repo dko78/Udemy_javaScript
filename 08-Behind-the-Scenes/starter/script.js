b5cc3a75ed6ca5a6a084e7f059ffab197a63f80c
Dalibor Kovacic <32059793+dko78@users.noreply.github.com>
2024-04-03 15:57:32 +0200

    kopije objekata

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 //https://github.com/jonasschmedtmann/complete-javascript-course
-
+/*
 const myName1 = 'Dalibor';
 
 function first1() {
@@ -52,12 +52,12 @@ console.log(me);
 
 var me = 'Dalibor';
 let job = 'Informatičar';
-const year = 1991;
+//const year = 1991;
 /*
 console.log(addDecl(2, 3));
 console.log(addExpr(2, 3));
 console.log(addArrow(2, 3));
-*/
+
 //function declaration
 
 function addDecl(a, b) {
@@ -86,7 +86,7 @@ function deleteShoppingChart() {
 
 //this keyword is not static, zavisi kako je funckija pozvana i vrijednost je poznata tek kad se pozove
 //arrow function nemaju this
-
+/*
 console.log(this);
 
 const calcAge1 = function (birthYear) {
@@ -115,3 +115,96 @@ matilda.calcAge(); //bitno je da razumiješ da se this odnosi na onu metodu s ko
 
 const f = jonas.calcAge; //met
 f(); //ovi ce biti undefined jer nema objekta na kojem se nalazi,ovo je samo funckija
+*/
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1978,
+  calcAge: function () {
+    //console.log(this); //to je ustvari jonas objekt
+    console.log(2024 - this.year);
+
+    const self = this; //self or that
+
+    //solution 1
+    /*
+    const isMillenial = function () {
+      //regulaan funcija dohvaća vlastiti this, ne od objekta
+      console.log(self);
+      //  console.log(this.year >= 1991 && this.year <= 1996);
+      console.log(self.year >= 1991 && self.year <= 1996);
+    };
+    
+      */
+    //solution 2 (arrow funcija nema svoju nego gleda od roditelja)
+    //arrow nem svoj this, nego uvijek dohvaća os svog roditelja
+    const isMillenial = () => {
+      console.log(this);
+      //  console.log(this.year >= 1991 && this.year <= 1996);
+      console.log(this.year >= 1991 && this.year <= 1996);
+    };
+
+    isMillenial(); //ovo je undefined jer kao da je poziv izvan objekta, a tada je ako se zove izvan objekta this undefinedd
+  },
+  greet: () => console.log(`Hej ${this.firstName}`), //preporuka je NE KORISTI ARROW funckija kaomethod. arrow funckija nema this, nego gleda od roditelja, što je ovdje global scope a tu nema firstName
+};
+
+jonas.greet(); //pozvano je sa global, pa će tu gledati this
+console.log(this.firstName);
+
+jonas.calcAge();
+
+// JS ENGINE : CALL STACK AND HEAP.
+// OBJECTS suspremljeni u HEAP
+
+//primitive types
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+//reference type
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+};
+
+const marriedJessica = jessica;
+
+marriedJessica.lastName = 'Davis';
+
+console.log('prije', jessica);
+console.log('poslije', marriedJessica);
+
+//                  copy
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2); //stvori novi objekt sa vrijednostima, ali samo shalow copy,jer ako ima property na 2 razini onda ne kpira dobro
+
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('prije', jessica2);
+console.log('poslije', jessicaCopy);
+
+//deep clone
+const jessica3 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy3 = structuredClone(jessica3);
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('prije 3', jessica3);
+console.log('poslije 3', jessicaCopy3);
