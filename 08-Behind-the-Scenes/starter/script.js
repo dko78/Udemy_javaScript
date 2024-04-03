9894437036f61b6183d6c467262c712f03520923
Dalibor Kovacic <32059793+dko78@users.noreply.github.com>
2024-04-03 13:41:36 +0200

    behind the scene primjeri

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -29,7 +29,8 @@ first1();
 //                                  scoping in practice
 //global scope funckija
 function calcAge(birthYear) {
-  //const firstName = 'Emina'; ako nazovešisto uzeti će ovu iz funckijene globals scop-a
+  //const firstName = 'Emina'; ako nazovešisto uzeti æe ovu iz funckijene globals scop-a
+  console.log(firstName);
   const age = 2024 - birthYear;
   //console.log(firstName); //first name nije definirana u funciji, alije def. na global scopu
   function printAge() {
@@ -42,3 +43,75 @@ function calcAge(birthYear) {
 
 const firstName = 'Dalibor';
 calcAge(1978);
+
+// HOISTING
+
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Dalibor';
+let job = 'Informatičar';
+const year = 1991;
+/*
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+*/
+//function declaration
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+//function expr.
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+//arrow function
+const addArrow = (a, b) => a + b;
+
+//example
+console.log('numProducts je:', numProducts);
+if (!numProducts) {
+  deleteShoppingChart();
+}
+
+var numProducts = 10;
+
+function deleteShoppingChart() {
+  console.log('All products deleted!');
+}
+
+//this keyword is not static, zavisi kako je funckija pozvana i vrijednost je poznata tek kad se pozove
+//arrow function nemaju this
+
+console.log(this);
+
+const calcAge1 = function (birthYear) {
+  console.log(2024 - birthYear);
+  console.log(this);
+};
+
+calcAge1(1978);
+
+const jonas = {
+  year: 1978,
+  calcAge: function () {
+    console.log(this); //to je ustvari jonas objekt
+    console.log(2024 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge; //method borowing -to niej poziv!
+matilda.calcAge(); //bitno je da razumiješ da se this odnosi na onu metodu s koje je pozoveš ne u kojoj je definirana. to nije static metoda
+
+const f = jonas.calcAge; //met
+f(); //ovi ce biti undefined jer nema objekta na kojem se nalazi,ovo je samo funckija
